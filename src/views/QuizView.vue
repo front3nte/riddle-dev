f5a8c7ff6749725780a14212450621bc3ed1123a
Front4nte <danielmorgenstern@duck.com>
2023-10-30 19:15:33 +0100

    Fix typo

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -67,12 +67,12 @@ const questions = [
       <h1>Herzlichen Glückwunsch!</h1>
       <p>
         Wir hoffen ihr hattet mächtigen Puzzle-Spaß. Euer Ehrgeiz und eure Sorgfalt hat euch hier
-        her geführt. Nun ihr habt tapfer gewartet bis diese Pforte sich öffnet. Doch bevor ihr
+        her geführt. Nun habt ihr tapfer gewartet bis diese Pforte sich öffnet. Doch bevor ihr
         endgültig am Ziel eurer Reise angekommen seid, warten noch ein paar Aufgaben auf euch, die
         es zu lösen gilt. Also nehmt einander an die Hand und macht euch auf die Suche nach der
         verborgenen Botschaft und durchquert das Rätseldickicht...
       </p>
-      <p>
+      <p class="wait-text">
         Doch bedenkt: die Weisheiten, die ihr auf dieser Reise sammeln werdet, werden noch von
         Belang sein, also bewahret sie sicher.
       </p>
