5a18ae02c9491cd7e19c8efc04a37aa923b47e09
Front4nte <danielmorgenstern@duck.com>
2023-10-30 03:29:44 +0100

    Iterate through questions

--- src/components/QuizApp.vue
+++ src/components/QuizApp.vue
@@ -4,23 +4,26 @@ import QuestionItem from './QuestionItem.vue'
 import { useQuizStore } from '../stores/quiz'
 
 const store = useQuizStore()
+
+const quizQuestions = [
+  {
+    q: 'Was ist die erste Ziffer von Jasmins Durchwahl?',
+    a: '8'
+  },
+  {
+    q: 'Wann hat Antonio auf seiner Tastatur einen echten Bug 🐞 gefunden?',
+    a: '15.12.2012'
+  },
+  {
+    q: 'Wann hat Daniel davon berichtet, dass eine Theme-Erstellerin Hühner, Feuer und Lamas in die IDE zaubert?',
+    a: '30.06.2021'
+  }
+]
 </script>
 
 <template>
   <h1>Naechstes Raetsel</h1>
-  <QuestionItem
-    v-if="store.count == 0"
-    question="Was ist die erste Ziffer von Jasmins Durchwahl?"
-    answer="8"
-  />
-  <QuestionItem
-    v-if="store.count == 1"
-    question="Wann hat Antonio auf seiner Tastatur einen echten Bug 🐞 gefunden?"
-    answer="15.12.2012"
-  />
-  <QuestionItem
-    v-if="store.count == 2"
-    question="Wann hat Daniel davon berichtet, dass eine Theme-Erstellerin Hühner, Feuer und Lamas in die IDE zaubert?"
-    answer="30.06.2021"
-  />
+  <template v-for="(item, index) in quizQuestions" :key="`test-${index}`">
+    <QuestionItem v-if="store.count == index" :question="item.q" :answer="item.a"/>
+  </template>
 </template>
